c6edec4c1eec61df57277d641cca6e4ff476404c
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-04-09 20:35:09 +0300

    Replace std::string with std::biset<32> in prefix_code_table

--- cccompression/algorithms/huffman_tree.hpp
+++ cccompression/algorithms/huffman_tree.hpp
@@ -3,12 +3,31 @@
 #include <queue>
 #include <memory>
 #include <vector>
+#include <bitset>
 
 #include "frequency_table.hpp"
 
+namespace {
+
+using CodeType = std::bitset<32>;
+
+struct PrefixCode {
+	CodeType value;
+	char pos;
+
+	PrefixCode next(int value) {
+		PrefixCode res = *this;
+		res.value.set(pos, value);
+		res.pos += 1;
+		return res;
+	}
+};
+
+}
+
 namespace eoanermine::cccompression {
 
-using prefix_code_table = std::unordered_map<char, std::string>;
+using prefix_code_table = std::unordered_map<char, CodeType>;
 
 struct huff_tree {
 	char element;
@@ -40,18 +59,18 @@ struct huff_tree {
 
 	prefix_code_table table() const {
 		prefix_code_table res;
-		encode(this, "", res);
+		encode(this, PrefixCode{}, res);
 		return res;
 	}
 private:
-	void encode(const huff_tree* root, std::string str, prefix_code_table& res) const {
+	void encode(const huff_tree* root, PrefixCode code, prefix_code_table& res) const {
 		if (!root->left && !root->right) {
-			res[root->element] = str;
+			res[root->element] = code.value;
 			return;
 		}
 
-		encode(root->left.get(), str + "0", res);
-		encode(root->right.get(), str + "1", res);
+		encode(root->left.get(), code.next(0), res);
+		encode(root->right.get(), code.next(1), res);
 	}
 };
 
